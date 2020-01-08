bb38d9b5b587e2b9e0996b70bef613a7faf2c56a
eduardo gonzalez <edugonzalezblanco@gmail.com>
2020-01-08 15:22:59 +0100

    Almacen DAO

--- gestorNoticias/src/main/java/com/viewnext/gestorNoticias/model/AlmacenDAONoticias.java
+++ gestorNoticias/src/main/java/com/viewnext/gestorNoticias/model/AlmacenDAONoticias.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 
+import com.viewnext.gestorNoticias.entities.Noticia;
+
 public interface AlmacenDAONoticias {
 
 	@Query(value="SELECT noticia.* FROM noticia_de_tema AS tu INNER JOIN noticia ON noticia.id = tu.id_noticia WHERE tu.id_tema = ?1", nativeQuery = true)
