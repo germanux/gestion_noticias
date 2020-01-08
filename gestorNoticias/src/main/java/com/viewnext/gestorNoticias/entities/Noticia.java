5de67095a966d084a8032163db071d45fed339f4
Javier Sánchez Morales <a@a.com>
2020-01-08 16:14:38 +0100

    Se añaden anotaciones

--- gestorNoticias/src/main/java/com/viewnext/gestorNoticias/entities/Noticia.java
+++ gestorNoticias/src/main/java/com/viewnext/gestorNoticias/entities/Noticia.java
@@ -1,11 +1,18 @@
 package com.viewnext.gestorNoticias.entities;
 
 import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Noticia {
@@ -15,11 +22,28 @@ public class Noticia {
 	private Integer id;
 	
 	@NotNull
+	@Size(min = 1, max = 100)
 	private String titular;
 	
 	@NotNull
+	@Size(min = 1, max = 500)
 	private String cabecera;
 	
+	@Temporal(TemporalType.DATE)
+	private Date fecha;
+	
+	@NotNull
+	@ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Integer idTema;
+	
+	public Integer getIdTema() {
+		return idTema;
+	}
+
+	public void setIdTema(Integer idTema) {
+		this.idTema = idTema;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -52,7 +76,6 @@ public class Noticia {
 		this.fecha = fecha;
 	}
 
-	private Date fecha;
 	
 	
 	
