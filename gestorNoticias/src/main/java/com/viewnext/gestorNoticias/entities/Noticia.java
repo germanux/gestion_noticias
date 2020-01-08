7bda7095f10cc3d0e1e6286cc3cc8bc3db2ff485
eduardo gonzalez <edugonzalezblanco@gmail.com>
2020-01-08 17:31:35 +0100

    Solucionado problema con html y app raiz

--- gestorNoticias/src/main/java/com/viewnext/gestorNoticias/entities/Noticia.java
+++ gestorNoticias/src/main/java/com/viewnext/gestorNoticias/entities/Noticia.java
@@ -3,12 +3,14 @@ package com.viewnext.gestorNoticias.entities;
 import java.util.Date;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -32,8 +34,7 @@ public class Noticia {
 	@Temporal(TemporalType.DATE)
 	private Date fecha;
 	
-	@NotNull
-	@ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Column(name="id_noticia_tema", insertable = true, updatable = true)
 	private Integer idTema;
 	
 	public Noticia(Integer id, @NotNull @Size(min = 1, max = 100) String titular,
