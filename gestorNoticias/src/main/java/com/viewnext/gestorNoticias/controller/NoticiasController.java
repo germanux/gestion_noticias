4b75f676dc2487d8a9dbda0c5ebce76253b26b78
Sergio Z <s.zapatero.mar@gmail.com>
2020-01-08 16:10:28 +0100

    Controlador de noticias con sus métodos CRUD, POM con el driver de MySQL, añadido a AlmacenDAO el JPA Repository

---
+++ gestorNoticias/src/main/java/com/viewnext/gestorNoticias/controller/NoticiasController.java
@@ -0,0 +1,56 @@
+package com.viewnext.gestorNoticias.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viewnext.gestorNoticias.entities.Noticia;
+import com.viewnext.gestorNoticias.model.AlmacenDAONoticias;
+
+@RestController()
+@RequestMapping("/api/noticias")
+@CrossOrigin("*")
+public class NoticiasController {
+
+	@Autowired
+	private AlmacenDAONoticias daoNoticias;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Noticia getNoticia(@PathVariable Integer id) {
+		return daoNoticias.findById(id).orElse(null);
+	}
+	
+	@PostMapping()
+	public Noticia crearNoticia(@RequestBody Noticia noticia) {
+		// Recibe sin ID en el BODY de la petición HTTP y deserializa el JSON a un objeto Usuario
+		return daoNoticias.save(noticia); // Devuelve con ID
+	}
+	
+	@GetMapping
+	public List<Noticia> leerTodos(){
+		return daoNoticias.findAll();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteNoticia(@PathVariable Integer id) {
+		//daoNoticiasTemasUsu.deleteByNoticia(id);
+		daoNoticias.deleteById(id);
+	}
+	
+	@PutMapping()
+	public Noticia modificarNoticia(@RequestBody Noticia noticia) {
+		return daoNoticias.save(noticia);
+	}
+
+}
--- gestorNoticias/src/main/java/com/viewnext/gestorNoticias/model/AlmacenDAONoticias.java
+++ gestorNoticias/src/main/java/com/viewnext/gestorNoticias/model/AlmacenDAONoticias.java
@@ -2,11 +2,12 @@ package com.viewnext.gestorNoticias.model;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.viewnext.gestorNoticias.entities.Noticia;
 
-public interface AlmacenDAONoticias {
+public interface AlmacenDAONoticias extends JpaRepository<Noticia, Integer>{
 
 	@Query(value="SELECT noticia.* FROM noticia_de_tema AS tu INNER JOIN noticia ON noticia.id = tu.id_noticia WHERE tu.id_tema = ?1", nativeQuery = true)
 	public List<Noticia> findNoticiasPorTema(Integer idTema);
