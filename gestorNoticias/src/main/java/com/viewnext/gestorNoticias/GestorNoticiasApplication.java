81745877ffaece978069e99f816f8b5b685aef7c
Javier Sánchez Morales <a@a.com>
2020-01-08 13:34:21 +0100

    Estructura inicial

---
+++ gestorNoticias/src/main/java/com/viewnext/gestorNoticias/GestorNoticiasApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.gestorNoticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorNoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorNoticiasApplication.class, args);
+	}
+
+}
